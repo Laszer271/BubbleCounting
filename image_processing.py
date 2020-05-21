62cf3b9c4d121d2367391ad51f615f739f191cf2
Lasze <Laszer2710@gmail.com>
2020-05-21 20:37:34 +0200

    update

---
+++ image_processing.py
@@ -0,0 +1,37 @@
+import os
+import collections
+import six
+
+def is_iterable(arg):
+    return (
+        isinstance(arg, collections.Iterable) 
+        and not isinstance(arg, six.string_types)
+    )
+
+
+def get_images_paths(input_path):
+    final_images_paths = []
+    
+    items = []
+    temp_items = []
+
+    if is_iterable(input_path):
+        temp_items.extend(input_path)
+    else:
+        temp_items.append(input_path)
+    
+    while len(temp_items):
+        items = temp_items
+        temp_items = []
+        
+        for item in items:
+            extension = item[-4:]
+            if extension != '.bmp':
+                # we assume that item is actually a directory
+                new_items = os.listdir(item)
+                for new_item in new_items:
+                    temp_items.append(item + f'/{new_item}')
+            else:
+                final_images_paths.append(item)
+    
+    return final_images_paths
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import numpy as np
 from PIL import Image
+import image_processing
 
 img = Image.open('pictures/001-s.bmp')
-img.show()
+#img.show()
+paths = image_processing.get_images_paths('pictures')
